371bba0af946d2aa0f034d25d0f749d5bc9c41bc
TotoCodex <123342029+TotoCodex@users.noreply.github.com>
2023-11-15 17:22:14 -0300

    Add files via upload

---
+++ fIREBASE32/fIREBASE32/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseEJ
+{
+    [FirestoreData]
+    public class Alumno
+    {
+        [FirestoreProperty]
+        public string Name { get; set; }
+        
+        public Alumno(): this(""){ }
+        public Alumno(string name)
+        {
+        }
+
+    }
+}
---
+++ fIREBASE32/fIREBASE32/Program.cs
@@ -0,0 +1,14 @@
+﻿using FirebaseEJ;
+
+namespace fIREBASE32
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            var fire = new Fire("prog-32");
+            await fire.Guardar(new FirebaseEJ.Alumno("JJUan"));
+            await fire.Leer2();
+        }
+    }
+}
